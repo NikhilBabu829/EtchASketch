2339bec32a02d610046086ace547824b98340395
Nikhil Gunti <nikhilbabu829@gmail.com>
2023-04-29 00:28:30 +0530

    done with the draw and erase function

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const main = document.querySelector('.main')
 
+const draw = document.querySelector('.draw')
+
+const erase = document.querySelector('.erase')
+
 const gridSizing = (size)=>{
     for(let i=0;i<size;i++){
         for(let j=0;j<size;j++){
@@ -10,3 +14,30 @@ const gridSizing = (size)=>{
 }
 
 gridSizing(16)
+
+const div = document.querySelectorAll('div')
+
+const drawing = ()=>{
+    for(let i=0;i<div.length;i++){
+        div[i].addEventListener('mousedown',()=>{
+            div[i].style.backgroundColor = 'black'
+        })
+    }
+}
+
+const erasing = ()=>{
+    for(let i=0;i<div.length;i++){
+        div[i].addEventListener('mousedown',()=>{
+            div[i].style.backgroundColor = 'white'
+        })
+    }
+}
+
+
+draw.addEventListener('click',()=>{
+    drawing()
+})
+
+erase.addEventListener('click',()=>{
+    erasing()
+})
